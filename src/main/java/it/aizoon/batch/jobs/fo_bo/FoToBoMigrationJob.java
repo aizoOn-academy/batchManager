b98bcb340bde41a30a1595d3dd986befb20007ab
Mr. EmPee <manuel.palumbo@email.com>
2022-07-29 10:37:05 +0200

    FoToBo fix

--- src/main/java/it/aizoon/batch/jobs/fo_bo/FoToBoMigrationJob.java
+++ src/main/java/it/aizoon/batch/jobs/fo_bo/FoToBoMigrationJob.java
@@ -1,14 +1,16 @@
 package it.aizoon.batch.jobs.fo_bo;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.SimpleJob;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Configuration;
 
 import it.aizoon.batch.jobs.fo_bo.model.Request;
 
-//@EnableBatchProcessing
-//@Configuration
+@Configuration
+@EnableBatchProcessing
 public class FoToBoMigrationJob extends SimpleJob {
     
     public FoToBoMigrationJob(JobRepository jobRepository, StepBuilderFactory stepBuilderFactory, FoReader reader, BoWriter writer) {
