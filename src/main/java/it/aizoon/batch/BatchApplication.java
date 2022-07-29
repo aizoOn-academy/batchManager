1327b3a75db82f6c718c947123e811396f10df72
Mr. EmPee <manuel.palumbo@email.com>
2022-07-29 10:50:46 +0200

    Fix Sequential Order Job Execution

--- src/main/java/it/aizoon/batch/BatchApplication.java
+++ src/main/java/it/aizoon/batch/BatchApplication.java
@@ -1,13 +1,44 @@
 package it.aizoon.batch;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BatchApplication {
 
-	public static void main(String[] args) {
-		System.exit(SpringApplication.exit(SpringApplication.run(BatchApplication.class, args)));
+	private final static List<Job> jobs = new ArrayList<>();
+
+	public static void main(String[] args) throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
+		ApplicationContext ctx = SpringApplication.run(BatchApplication.class, args);
+
+		jobs.add(
+			ctx.getBean("foToBoMigrationJob", Job.class)
+		);
+
+		jobs.add(
+			ctx.getBean("boToReMigrationJob", Job.class)
+		);
+
+		JobLauncher jobLauncher = (JobLauncher) ctx.getBean("jobLauncher");
+		for(Job job : jobs) {
+			JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("startAt", System.currentTimeMillis()).toJobParameters();
+
+			jobLauncher.run(job, jobParameters);
+		}
+
 	}
 
 }
